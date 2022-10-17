b91b58f0cd9cdb9437d8be49758d2e9cf488af53
LuisFelipe2 <luisfelipe.auth@gmail.com>
2022-10-17 18:04:27 -0300

    Add a simple camel to print hello world for test

--- src/main/java/com/auth/authreformas/core/facade/gallery/GalleryFacade.java
+++ src/main/java/com/auth/authreformas/core/facade/gallery/GalleryFacade.java
@@ -1,5 +1,8 @@
 package com.auth.authreformas.core.facade.gallery;
 
+import com.auth.authreformas.router.MainRouter;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -12,6 +15,15 @@ public class GalleryFacade {
 		List<String> gallery = new ArrayList<>();
 		gallery.add("Filipe");
 		gallery.add("Charles");
+
+		try (CamelContext camel = new DefaultCamelContext()) {
+			camel.addRoutes(new MainRouter());
+			camel.start();
+			Thread.sleep(10_000);
+			camel.stop();
+		} catch (Exception e) {
+			System.out.println("error happened");
+		}
 
 		return gallery;
 	}
